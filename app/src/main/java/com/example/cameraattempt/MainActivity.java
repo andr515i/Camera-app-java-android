c7e8bd8116b4d4456a8ed4411127d72989c52973
andr515i <andr515i@zbc.dk>
2022-06-15 14:51:43 +0200

    app now functioning, though it stores pictures in very low resolution

--- app/src/main/java/com/example/cameraattempt/MainActivity.java
+++ app/src/main/java/com/example/cameraattempt/MainActivity.java
@@ -1,11 +1,20 @@
 package com.example.cameraattempt;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
     //init vars
@@ -21,9 +30,32 @@ public class MainActivity extends AppCompatActivity {
         //assign vars
         imageView = findViewById(R.id.image_view);
         btOpen = findViewById(R.id.bt_open);
-
+        // test
         //request for camera permission
-        
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{
+                            Manifest.permission.CAMERA
+                    }, 100);
+        }
+        btOpen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //open camera
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 100);
+            }
+        });
+    }
 
+    @SuppressLint("MissingSuperCall")
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+        if (requestCode == 100) {
+            //get capture image
+            Bitmap captureImage = (Bitmap) data.getExtras().get("data");
+            // set captured image to imageview
+            imageView.setImageBitmap(captureImage);
+        }
     }
 }
